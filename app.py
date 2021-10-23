dc91c97ee916ad07d3af800830e2584fc7ae94c3
Murilo Gonçalves <murilogoncalves127@gmail.com>
2021-10-22 21:10:41 -0300

    almost

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def stream():
     tp.start()
     def get_latest_ping():
         while True:
-            yield str(ping_buffer[BUFFER_SIZE-1]) + " "
+            yield str(ping_buffer[BUFFER_SIZE-1])
             sleep(1)
 
     return app.response_class(get_latest_ping(), mimetype="text/plain")
